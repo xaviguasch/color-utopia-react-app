379cfd6946a4c68c10dd6b5134c15162d6d0f058
xaviguasch <xaviguasch@gmail.com>
2019-05-20 20:52:25 +0200

    refactored ColorBox styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -8,13 +8,9 @@ class SingleColorPalette extends Component {
   constructor(props) {
     super(props)
     this._shades = this.gatherShades(this.props.palette, this.props.colorId)
-    this.state = {
-      format: 'hex'
-    }
-
+    this.state = { format: 'hex' }
     this.changeFormat = this.changeFormat.bind(this)
   }
-
   gatherShades(palette, colorToFilterBy) {
     let shades = []
     let allColors = palette.colors
@@ -22,26 +18,27 @@ class SingleColorPalette extends Component {
     for (let key in allColors) {
       shades = shades.concat(allColors[key].filter(color => color.id === colorToFilterBy))
     }
-
     return shades.slice(1)
   }
-
   changeFormat(val) {
     this.setState({ format: val })
   }
-
   render() {
     const { format } = this.state
     const { paletteName, emoji, id } = this.props.palette
     const colorBoxes = this._shades.map(color => (
-      <ColorBox key={color.name} name={color.name} background={color[format]} showLink={false} />
+      <ColorBox
+        key={color.name}
+        name={color.name}
+        background={color[format]}
+        showingFullPalette={false}
+      />
     ))
     return (
       <div className='SingleColorPalette Palette'>
         <Navbar handleChange={this.changeFormat} showingAllColors={false} />
         <div className='Palette-colors'>
           {colorBoxes}
-
           <div className='go-back ColorBox'>
             <Link to={`/palette/${id}`} className='back-button'>
               GO BACK
@@ -53,5 +50,4 @@ class SingleColorPalette extends Component {
     )
   }
 }
-
 export default SingleColorPalette
