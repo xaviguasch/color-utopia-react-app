377d6796ff8f86d4306b9d4df073020fe9f6ec31
xaviguasch <xaviguasch@gmail.com>
2019-05-21 11:32:57 +0200

    Add NewPaletteForm and Link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PaletteList from './PaletteList'
 import SingleColorPalette from './SingleColorPalette'
 import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
+import NewPaletteForm from './NewPaletteForm'
 
 class App extends Component {
   findPalette(id) {
@@ -15,6 +16,8 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
+
         <Route
           exact
           path='/palette/:paletteId/:colorId'
